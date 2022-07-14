12396f393555184e05a265f88f6f4cad2da44704
ostap_sulyk <ostapsulyk@gmail.com>
2022-07-14 13:07:33 -0400

    added documentation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use dirs::home_dir;
 use std::fs;
+/// if you want to conveniently sort 
+/// ```
+/// .config/nvim/spell/en.utf-8.add
+/// ```
+/// file
+/// and you want to sort words in that file,
+/// you can install this binary and it should work
+///
+/// i am still new to rust and i know that this code is really unoptimized
+/// so if you know how to improve it just do a pull request
+///
+/// # Example
+/// after installing 'sort_nvim_spell_file'
+/// just type it in terminal and it should sort your file
+/// ```
+/// sort_nvim_spell_file
+/// ```
+/// and press enter
+/// this should sort your dictionary file 
 
 fn main() -> std::io::Result<()> {
     let user_home = home_dir().unwrap();
     let mut user_home: String = user_home.to_str().unwrap().to_owned();
+
     user_home.push_str("/.config/nvim/spell/en.utf-8.add");
+
     let path_to_spell_file: &str = user_home.as_str();
+
     let mut words_list: Vec<String> = fs::read_to_string(path_to_spell_file)?
         .split('\n')
         .map(|x| x.to_string())
